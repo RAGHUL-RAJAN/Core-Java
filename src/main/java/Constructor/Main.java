a575561a37ab455e2b497d9a16a8cc3adedd7bd3
RAGHUL-RAJAN <raghulrajan2015@gmail.com>
2025-04-17 12:42:56 +0530

    Constructor

---
+++ src/main/java/Constructor/Main.java
@@ -0,0 +1,8 @@
+package Constructor;
+
+public class Main{
+    public static void main(String[] args) {
+        Student st = new Student("Jack");
+        st.isDisplay();
+    }
+}
---
+++ src/main/java/Constructor/Student.java
@@ -0,0 +1,14 @@
+package Constructor;
+
+public class Student {
+    String name;
+
+    Student(String n){
+        name = n;
+    }
+
+   void isDisplay(){
+        System.out.println("Name of the student is "+ name);
+    }
+}
+
