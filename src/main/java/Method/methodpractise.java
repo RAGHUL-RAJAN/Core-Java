f0bc9cc28bdab87d67dd4352bf3852c59b34c8c3
RAGHUL-RAJAN <raghulrajan2015@gmail.com>
2025-05-01 23:21:29 +0530

    Method

---
+++ src/main/java/Method/methodpractise.java
@@ -0,0 +1,17 @@
+package Method;
+
+public class methodpractise {
+//    1. No parameter & No return value
+//    2. No parameter & with return value
+//    3. Take parameter & No return value
+//    4. Take parameter & with return value
+
+    void m1(){
+        System.out.println("Hello world");
+    }
+
+    public static void main(String[] args) {
+        methodpractise mp = new methodpractise();
+        mp.m1();
+    }
+}
