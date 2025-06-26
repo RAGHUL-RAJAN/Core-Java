22b8e1f60b582cacfbfdbb062e617d4f9b002c77
RAGHUL-RAJAN <raghulrajan2015@gmail.com>
2025-06-26 11:53:59 +0530

    Problems solving

---
+++ src/main/java/Problems/CountNumber.java
@@ -0,0 +1,17 @@
+package Problems;
+
+public class CountNumber {
+
+    public static void main(String[] args) {
+        int num = 12345;
+
+        int rev = 0;
+
+        while(num>0){
+            rev = rev + num%10;
+            num = num/10;
+        }
+
+        System.out.println("The sum of the number is : "+ rev);
+    }
+}
