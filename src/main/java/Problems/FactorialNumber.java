1d6c2c172906d4bbdba9247dc547a792662ead92
RAGHUL-RAJAN <raghulrajan2015@gmail.com>
2025-07-02 10:46:45 +0530

    changes

--- src/main/java/Problems/FactorialNumber.java
+++ src/main/java/Problems/FactorialNumber.java
@@ -2,11 +2,19 @@ package Problems;
 
 public class FactorialNumber {
     public static void main(String[] args) {
-       int num = 5, factorial = 1;
-       for(int i = 1; i<= num;i++){
-           factorial = factorial*i;
-       }
+//       int num = 5, factorial = 1;
+//       for(int i = 1; i<= num;i++){
+//           factorial = factorial*i;
+//       }
+//
+//        System.out.println(factorial);
 
+        int num = 5;
+        int factorial = 1;
+
+        for(int i =1; i <= num ; i++){
+            factorial = factorial * i;
+        }
         System.out.println(factorial);
     }
 }
