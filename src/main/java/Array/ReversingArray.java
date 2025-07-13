dbf06c19c3c5f7672d1b9d6cb0479d087ba22881
RAGHUL-RAJAN <raghulrajan2015@gmail.com>
2025-07-13 20:42:40 +0530

    changes

--- src/main/java/Array/ReversingArray.java
+++ src/main/java/Array/ReversingArray.java
@@ -3,7 +3,6 @@ package Array;
 public class ReversingArray {
     public static void main(String[] args) {
         int a[] = {2,3,4,5,6,7};
-
         for(int i =a.length-1; i >=0;i--){
             System.out.println(a[i]);
         }
