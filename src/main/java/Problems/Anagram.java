2e421c21829f95c8ac30adc3f6dc444eba218c5c
RAGHUL-RAJAN <raghulrajan2015@gmail.com>
2025-07-24 20:23:56 +0530

    Anagram code

--- src/main/java/Problems/Anagram.java
+++ src/main/java/Problems/Anagram.java
@@ -13,10 +13,8 @@ public class Anagram {
     public static boolean checkAnagram(String a, String b){
         char[] a1 = a.toCharArray();
         char[] b1 = b.toCharArray();
-
         Arrays.sort(a1);
         Arrays.sort(b1);
-
         return Arrays.equals(a1,b1);
     }
 }
