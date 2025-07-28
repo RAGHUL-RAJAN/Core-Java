a5c237e392a3ba58a132c8d26d32825ea4f3a91e
RAGHUL-RAJAN <raghulrajan2015@gmail.com>
2025-07-28 14:27:46 +0530

    changes

--- src/main/java/Collection/ArList.java
+++ src/main/java/Collection/ArList.java
@@ -5,8 +5,13 @@ public class ArList {
         ArrayList<String> st = new ArrayList<>();
         st.add("Jack");
         st.add("car");
-
         System.out.println(st);
         System.out.println(st.get(1));
     }
 }
+/*
+    wait<WebDriver> fluentWait = new FluentWait<>(driver)
+        .withTimeout(Duration.ofSeconds(30));
+        .pollingEvery(Duration.ofSeconds(5));
+        .ignore(NoSuchElementException.class);
+ */
